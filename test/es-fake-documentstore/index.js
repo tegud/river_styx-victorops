d5abbf422c476d1d693f58c112ed62d2b1b358a2
Steve Elliott <steve@tegud.net>
2015-09-26 22:09:47 +0100

    Tests
    Abandoned nock, custom http server

---
+++ test/es-fake-documentstore/index.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var Promise = require('bluebird');
+
+module.exports = function() {
+	var indicies = {};
+
+	var documentRegex = /\/([^\/^?]+)\/([^\/^?]+)\/([^\/^?]+)(\/( [^\/^?]+))?/i;
+
+	var server = http.createServer(function(request, response) {
+		var urlMatches = request.url.match(documentRegex);
+		var index = urlMatches[1];
+		var type = urlMatches[2];
+		var id = urlMatches[3];
+		var method = urlMatches[5];
+
+		if(!indicies[index]) {
+			indicies[index] = {};
+		}
+		if(!indicies[index][type]) {
+			indicies[index][type] = {};
+		}
+
+		var body = '';
+
+		request.on('data', function(chunk) {
+			body += chunk;
+		});
+
+		request.on('end', function() {
+			var parsedBody = JSON.parse(body);
+			if(!indicies[index][type][id]) {
+				indicies[index][type][id] = parsedBody.doc;
+			}
+			
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.end();
+		});
+	});
+
+	return {
+		start: function() {
+			server.listen(9200);
+		},
+		get: function(index, type, id) {
+			return new Promise(function(resolve, reject) {
+				if(!indicies[index]) {
+					console.log('*************');
+					console.log(indicies);
+					return reject('Index missing');
+				}
+
+				if(!indicies[index][type]) {
+					return reject('Type missing');
+				}
+
+				if(!indicies[index][type][id]) {
+					return reject('Document missing');
+				}
+
+				resolve(indicies[index][type][id]);
+			});
+
+		},
+		reset: function() {
+
+		},
+		stop: function() {
+
+		}
+	}
+};
--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ var proxyquire = require('proxyquire');
 var amqpSub = require('./amqp-sub');
 var fs = require('fs');
 var nock = require('nock');
+var logger = require('../lib/logger').forModule('Integration Test');
 var App = proxyquire('../lib/server', {
 	'./config': function() {
 		return {
@@ -15,10 +16,31 @@ var App = proxyquire('../lib/server', {
 		};
 	}
 });
+var FakeEsSocumentStore = require('./es-fake-documentstore');
+
+function waitFor(time) {
+	return function() {
+		return new Promise(function(resolve, reject) {
+			setTimeout(function() { resolve(); }, time);
+		});
+	};
+}
 
 describe('river_styx-victorops', function() {
+	var fakeEsSocumentStore;
+
+	beforeEach(function() {
+		fakeEsSocumentStore = new FakeEsSocumentStore();
+		fakeEsSocumentStore.start();
+
+	});
+
+	afterEach(function() {
+		fakeEsSocumentStore.stop();
+	});
+
 	describe('victorops webhook alert', function() {
-		it('is stored to elasticsearch', function(done) {
+		it.only('is stored to elasticsearch', function(done) {
 			var mockAmqpServer = amqpSub.mock({ host: '127.0.0.1', port: 5672 })
 			inputExchange = mockAmqpServer.exchange('river-styx');
 
@@ -49,24 +71,96 @@ describe('river_styx-victorops', function() {
 			    "team": "Unknown"
             };
 
+			new App({
+					elasticsearch: {
+						host: 'localhost'
+					}
+				})
+				.start()
+				.then(function() { 
+					inputExchange.publish('', fs.readFileSync(__dirname + '/data/justalert.json')); 
+				})
+				.then(waitFor(100))
+				.then(fakeEsSocumentStore.get.bind(undefined, 'releases-2015.09', 'victoropsAlert', 'b295e252-67f2-4317-ab51-fa2856f4fb2d'))
+				.then(function(storedDocument) {
+					expect(storedDocument).to.eql(expectedAlertEsRecord);
+					done();
+				})
+				.catch(function(e) {
+					expect(e).to.be(null); 
+				});
+		});
+	});
+
+	describe('victorops webhook and hubot', function() {
+		it('incident name is stored to elasticsearch', function(done) {
+			var mockAmqpServer = amqpSub.mock({ host: '127.0.0.1', port: 5672 })
+			inputExchange = mockAmqpServer.exchange('river-styx');
+
+			var expectedAlertEsRecord = {
+			    "incident": 7794913,
+			    "monitoringTool": "NewRelic",
+			    "stateStartTime": "2015-08-23T00:10:13+00:00",
+			    "@timestamp": "2015-08-23T00:10:13+00:00",
+			    "notificationType": "PROBLEM",
+			    "voAlertType": "SERVICE",
+			    "voAlertReceivedTime": "2015-08-23T00:10:13+00:00",
+			    "entityDisplayName": "Apdex score &lt; 0.7",
+			    "messageType": "CRITICAL",
+			    "alertType": "PROBLEM",
+			    "hostName": "\"\"",
+			    "voUuid": "b295e252-67f2-4317-ab51-fa2856f4fb2d",
+			    "entityState": "CRITICAL",
+			    "ackAuthor": "\"\"",
+			    "entityId": "NewRelic-Apdex score &lt; 0.7-17039499-",
+			    "stateMessage": "New alert for BookingWeb: Apdex score &lt; 0.7",
+			    "icingaInstance": "NewRelic",
+			    "serviceDesc": "NewRelic-Apdex score &lt; 0.7-17039499-",
+			    "message": "Apdex score &lt; 0.7",
+			    "entityIsHost": "false",
+			    "routingKey": "newRelic",
+			    "voMonitorType": 6,
+			    "summary": "PROBLEM  NewRelic-Apdex score &lt; 0.7-17039499- CRITICAL",
+			    "team": "Unknown"
+            };
+
+            var expectedNameEsUpdate = {
+			    voUuid: "b295e252-67f2-4317-ab51-fa2856f4fb2d",
+            	incidentName: '1234'
+            };
+
 			var storeToEs = nock('http://localhost:9200')
                 .post('/releases-2015.09/victoropsAlert/b295e252-67f2-4317-ab51-fa2856f4fb2d/_update?retry_on_conflict=3', {
                 	doc: expectedAlertEsRecord,
                 	upsert: expectedAlertEsRecord
                 })
-                .reply(200, {});
+                .reply(200, { })
+                .post('/releases-2015.09/victoropsAlert/b295e252-67f2-4317-ab51-fa2856f4fb2d/_update?retry_on_conflict=3', {
+                	doc: expectedNameEsUpdate,
+                	upsert: expectedNameEsUpdate
+                })
+                .reply(200, { })
+                .log(console.log);
 
 			new App({
-				elasticsearch: {
-					host: 'localhost'
-				}
-			}).start()
+					elasticsearch: {
+						host: 'localhost'
+					}
+				})
+				.start()
 				.then(function() { 
-					inputExchange.publish('', fs.readFileSync(__dirname + '/data/justalert.json')); 
+					var events = JSON.parse(fs.readFileSync(__dirname + '/data/alertThenName.json'));
+
+					while(events.length) {
+						inputExchange.publish('', JSON.stringify(events.shift())); 
+					}
 				})
 				.then(function() {
-					storeToEs.done();
-					done();
+					setTimeout(function() {
+						storeToEs.done();
+						console.log('>>>>>>>> Test Complete');
+						done();
+					}, 1000);
 				});
 		});
 	});
